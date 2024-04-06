1af18dcec8a7b3ee1a8546124ea16bcf8c3c3979
Pac12Run8000 <gangstar78666@>
2024-04-06 16:55:05 -0700

    Initial Commit

---
+++ DisplayListAsyncAwaitWithProtocol/DisplayListAsyncAwaitWithProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayListAsyncAwaitWithProtocolApp.swift
+//  DisplayListAsyncAwaitWithProtocol
+//
+//  Created by Norbert Grover on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DisplayListAsyncAwaitWithProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
