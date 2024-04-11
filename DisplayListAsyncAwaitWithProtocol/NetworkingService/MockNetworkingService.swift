25b3654daf9c1e9a41e0f7b8141eb90fdf08987c
Pac12Run8000 <gangstar78666@>
2024-04-11 10:26:45 -0700

    Modified the MockNetworkingService

--- DisplayListAsyncAwaitWithProtocol/NetworkingService/MockNetworkingService.swift
+++ DisplayListAsyncAwaitWithProtocol/NetworkingService/MockNetworkingService.swift
@@ -1,14 +1,19 @@
 import Foundation
 
 final class MockNetworkingService: NetworkingServiceProtocol {
-    
+    var mockWordList: [String]? = []
+    var shouldThrowError:CustomErrors?
    
     func retrieveData(url: URL) async throws -> [String] {
+        if let error = shouldThrowError {
+            throw error
+        }
         guard url != URL(string: "http://www.nactem.ac.uk/software/acromine/dictionary.py?sf=") else {
             throw CustomErrors.badURL
         }
-        
-        var wordList = [String]()
+        if let wordList = mockWordList {
+            return wordList
+        }
         guard let url = try Bundle.main.url(forResource: "JsonResponse", withExtension: "geojson"), let data = try Data(contentsOf: url) as? Data else {
             throw CustomErrors.noData
         }
@@ -20,7 +25,7 @@ final class MockNetworkingService: NetworkingServiceProtocol {
         guard !acronymList.isEmpty else {
             throw CustomErrors.emptyObject
         }
-        
+        var wordList = [String]()
         for item in acronymList[0].lfs {
             wordList.append(item.lf)
         }
